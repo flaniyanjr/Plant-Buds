9e14a074e131ce9eb92177b296557be4dd0f02c0
Hannah Schwaeble <hannah.schwaeble@gmail.com>
2023-11-30 11:38:24 -0500

    merge issues

--- client/src/components/OwnerCard.js
+++ client/src/components/OwnerCard.js
@@ -4,7 +4,7 @@ function OwnerCard({id, name , onClick}) {
 
     
     return (
-        <div className="card">
+        <div className="owner-card">
             <h2>{name}</h2>
             <button onClick={() => onClick(name)}>View user's plants</button>
         </div>
